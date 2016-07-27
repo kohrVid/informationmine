15d9979400b1ebb8d9d5e0e3a4111f959e95f50b
kohrVid <kohrVid@zoho.com>
2016-07-27 22:02:26 +0100

    Refactored views

--- app/indices/posts.rb
+++ app/indices/posts.rb
@@ -1,9 +1,9 @@
 ThinkingSphinx::Index.define :post, with: :active_record do
   indexes title
   indexes body
-=begin
   set_property morphology: stem_en
-  set_property min_infix_len: 2
+  set_property enable_star: true
+  set_property min_infix_len: 3
+  set_property min_stemming_len: 3
   has created_at, updated_at
-=end
 end
